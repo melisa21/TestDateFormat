103d68ac909224ed25f912d97b3d88bc9334b301
melisa21 <62158035+melisa21@users.noreply.github.com>
2020-04-21 15:41:00 -0300

    Actualizacion Test1

--- src/Test/Library.Test/UnitTest1.cs
+++ src/Test/Library.Test/UnitTest1.cs
@@ -4,15 +4,20 @@ namespace Library.Test
 {
     public class Tests
     {
+
+        private string date;
         [SetUp]
         public void Setup()
         {
+            string testDate = "10/11/1977";
+            //devuelve la fecha en formato “aaaa­‐mm­‐dd”
+            date = testDate.Substring(6) + "-" + testDate.Substring(3, 2) + "-" + testDate.Substring(0, 2);
         }
 
         [Test]
         public void Test1()
         {
-            Assert.Pass();
+            Assert.AreEqual("1977-11-10", date);
         }
     }
 }
